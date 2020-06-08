f22739307b333c51b0dd7d4be30d6e1bd3ff1e31
3tbraden <wongbradenyinlok@gmail.com>
2020-06-08 22:49:38 +0800

    chore: build and demo

---
+++ build/precache-manifest.3b58d0495f1feadcb5f0cb8fb9a63f41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c2eefef556c99e2f34169df96acc5aa",
+    "url": "/solar-system-animation/index.html"
+  },
+  {
+    "revision": "2f6f554b83919ce13616",
+    "url": "/solar-system-animation/static/css/main.5f732f59.chunk.css"
+  },
+  {
+    "revision": "f73b35a94a64fc1f5495",
+    "url": "/solar-system-animation/static/js/2.a4548b93.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/solar-system-animation/static/js/2.a4548b93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f6f554b83919ce13616",
+    "url": "/solar-system-animation/static/js/main.e439ea6d.chunk.js"
+  },
+  {
+    "revision": "4dd75859921f5e27db13",
+    "url": "/solar-system-animation/static/js/runtime-main.d7727914.js"
+  }
+]);
